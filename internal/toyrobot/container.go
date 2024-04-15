e6c52351f77c541ac4cf81a8af47bb8a1dc4ecb1
Isai Alcântara <isfalcantara@gmail.com>
2024-04-15 15:21:19 -0300

    remove addObject from container interface
    addObject is not called from a container, only from the table's
    internals, therefore it doesn't need to be in the interface.

--- internal/toyrobot/container.go
+++ internal/toyrobot/container.go
@@ -2,7 +2,6 @@ package toyrobot
 
 type container interface {
 	placeObject(Object, Transform) error
-	addObject(Object, Transform) error
 	removeObject(Object) error
 	objectTransform(Object) (Transform, error)
 	moveObject(MovableObject) error
